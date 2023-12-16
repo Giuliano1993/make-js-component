8e4f69ba6cd00904d8e055cc797f4611124ce390
giuseppesciacca <sciaccagi@gmail.com>
2023-12-16 20:06:42 +0100

    FIX bug when creating react component with styled-components. Closes #86

--- src/utils/frameworks/react/react.mjs
+++ src/utils/frameworks/react/react.mjs
@@ -1,50 +1,50 @@
 import inquirer from "inquirer";
 const framework = "react";
 export default function (componentName, folder) {
-    return inquirer
-        .prompt([
-        {
-            type: "confirm",
-            name: "typescript",
-            message: "Do you want to use Typescript?",
-            default: true,
-        },
+  return inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "typescript",
+        message: "Do you want to use Typescript?",
+        default: true,
+      },
     ])
-        .then((answers) => {
-        const { typescript } = answers;
-        return inquirer
-            .prompt([
-            {
-                type: "list",
-                name: "css",
-                message: "Do you want to use any CSS framework?",
-                choices: ["Tailwind", "Styled Components", "CSS Module", "No"],
-            },
+    .then((answers) => {
+      const { typescript } = answers;
+      return inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "css",
+            message: "Do you want to use any CSS framework?",
+            choices: ["Tailwind", "Styled Components", "CSS Module", "No"],
+          },
         ])
-            .then((answers) => {
-            const { css } = answers;
-            const extension = typescript ? "tsx" : "jsx";
-            let templateBase = "function-component";
-            switch (css) {
-                case "Tailwind":
-                    templateBase += "-tailwind";
-                    break;
-                case "Styled Components":
-                    templateBase += "-styled";
-                    break;
-                case "CSS Module":
-                    templateBase += "-css-module";
-                    break;
-                default:
-                    break;
-            }
-            const template = `${templateBase}.${extension}`;
-            return {
-                componentName: componentName,
-                framework: framework.toLowerCase(),
-                template: template,
-                folder: folder,
-            };
+        .then((answers) => {
+          const { css } = answers;
+          const extension = typescript ? "tsx" : "jsx";
+          let templateBase = "function-component";
+          switch (css) {
+            case "Tailwind":
+              templateBase += "-tailwind";
+              break;
+            case "Styled Components":
+              templateBase += "-styled-components";
+              break;
+            case "CSS Module":
+              templateBase += "-css-module";
+              break;
+            default:
+              break;
+          }
+          const template = `${templateBase}.${extension}`;
+          return {
+            componentName: componentName,
+            framework: framework.toLowerCase(),
+            template: template,
+            folder: folder,
+          };
         });
     });
 }
