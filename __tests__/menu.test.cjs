3d7fe83995f10959ba558158e4596ef8805337e9
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2024-01-19 23:11:34 +0100

    add fs to menu.test

--- __tests__/menu.test.cjs
+++ __tests__/menu.test.cjs
@@ -1,6 +1,7 @@
 const {render} = require('cli-testing-library');
 const { default: exp } = require('constants');
 const {resolve} = require('path')
+const fs = require('fs')
 
 
 
@@ -36,5 +37,28 @@ test('Default folder and pick React', async () => {
   userEvent.keyboard('[ArrowDown]')
   userEvent.keyboard('[ArrowDown]')
   expect(await findByText('❯ React')).toBeInTheConsole()
+
+})
+
+test('Create React Component', async () => {
+  const path = resolve(__dirname, "../cmd/make-js-component.mjs")
+  const {clear, findByText, queryByText, userEvent, stdoutArr, debug} = await render('node', [
+    path
+  ])
+  
+  userEvent.keyboard('componentName[Enter]');
+  expect(await findByText('src/components')).toBeInTheConsole()
+  userEvent.keyboard('[Enter]');
+  userEvent.keyboard('[ArrowDown]')
+  userEvent.keyboard('[ArrowDown]')
+  expect(await findByText('❯ React')).toBeInTheConsole()
+  userEvent.keyboard('[Enter]');
+  expect(await findByText('Typescript?')).toBeInTheConsole()
+  userEvent.keyboard('Y[Enter]');
+  expect(await findByText('❯ No')).toBeInTheConsole()
+  userEvent.keyboard('[Enter]');
+  expect(await findByText('✅ CREATE Component: src\components\componentName.tsx')).toBeInTheConsole()
+  const componentExsists = fs.existsSync(resolve(__dirname, '../src/components/componentName.tsx'));
+  expect(componentExsists).toBe(true)
 
 })
